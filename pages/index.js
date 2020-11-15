818f3f33278fe926516a6c90bee341cb63ecadb5
Eugene Avila <eugene@ctoglobal.co>
2020-11-16 00:17:40 +0800

    master - added less module - made basic layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.less'
+import { faUser, faHome, faGraduationCap, faBriefcase, faPhoneAlt, faCopyright } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Home () {
   return (
     <div className={styles.container}>
-      Eugene Avila
+      <div className={styles.outerControls}>
+        <div className={styles.mainControl}>
+          <FontAwesomeIcon icon={faHome} className={styles.faIcons} />
+          <FontAwesomeIcon icon={faUser} className={styles.faIcons} />
+          <FontAwesomeIcon icon={faGraduationCap} className={styles.faIcons} />
+          <FontAwesomeIcon icon={faBriefcase} className={styles.faIcons} />
+          <FontAwesomeIcon icon={faPhoneAlt} className={styles.faIcons} />
+        </div>
+      </div>
+      <div className={styles.innerBox}>
+        <section className={styles.rightContent}>
+          <div className={styles.profileSection}>
+            <div className={styles.profileImg} />
+            <div className={styles.headingText}>Eugene Avila</div>
+            <div className={styles.userTitle}>Senior Developer</div>
+            <div className={styles.socialIcons}>
+              <FontAwesomeIcon icon={faFacebookF} />
+              <FontAwesomeIcon icon={faLinkedin} />
+            </div>
+          </div>
+          <div className={styles.dowloadBtn}>
+            <button>Download CV</button>
+          </div>
+          <footer className={styles.footer}>
+            <FontAwesomeIcon icon={faCopyright} /> 2020 All rights reserved.
+          </footer>
+        </section>
+        <div className={styles.mainContent}>Main Content</div>
+      </div>
     </div>
   )
 }

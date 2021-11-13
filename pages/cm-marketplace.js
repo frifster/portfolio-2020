579a6b8de7bf060b45822a2af87f10c488d44f7f
Eugene Avila <frifster2014@gmail.com>
2021-11-13 18:21:49 +0800

    Added pesos conversion and fixed some intervals

--- pages/cm-marketplace.js
+++ pages/cm-marketplace.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Head from "next/head";
 import styles from '../styles/CMarket.module.less'
 
 const getEternalPrice = async () => {
@@ -13,25 +14,54 @@ const getEternalPrice = async () => {
 const ADD_TAX = 1.17647
 
 function Market() {
-    const [ETLPrice, setETLPrice] = useState(0);
+    const [ETLPrice, setETLPrice] = useState(0)
     const [mintCost, setMintCost] = useState(0)
 
-    useEffect(() => {
+    const setEternalPrice = () => {
         getEternalPrice()
             .then(price => {
+                console.log("SETTING PRICE")
                 const mintCost = (20 / price).toFixed(4);
                 setMintCost(mintCost)
                 setETLPrice(Number(price).toFixed(4))
             })
+    }
+
+    useEffect(() => {
+        setEternalPrice()
     }, [])
 
     useEffect(() => {
-          const interval = setInterval(getEternalPrice, 10000);
+          const interval = setInterval(setEternalPrice, 10000);
           return () => clearInterval(interval);
       }, []);
 
+    
+    const computeForMarketPrice = multiplier => {
+        return (mintCost * ADD_TAX * multiplier).toFixed(2)
+    }
+
+    const computeForPHPMarketPrice = multiplier => {
+        const formatter = new Intl.NumberFormat('en-US', { currency: 'PHP', style: 'currency' })
+        return formatter.format((mintCost * ADD_TAX * multiplier * ETLPrice * 50).toFixed(2))
+    }
+
+    const ETLPriceDisplay = ({ multiplier }) => {
+        const price = computeForMarketPrice(multiplier)
+        const PHPPrice = computeForPHPMarketPrice(multiplier)
+
+        return (
+            <p className={styles.ETLPriceWorker}>{price} ETL ({PHPPrice})</p>
+        )
+    }
+
     return (
         <div className={styles.container}>
+            <Head>
+                <title>CM Marketplace Price Estimates</title>
+                <link rel="icon" href="./icon.png" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <div className={styles.headers}>
                 <p><span>ETL :</span> $ {ETLPrice}</p>
                 <p><span>Mint Cost :</span> {mintCost} ETL</p>
@@ -42,71 +72,75 @@ function Market() {
                 <div className={styles.rarity}>
                     <div className={styles.rareStar}>⭐⭐⭐⭐⭐</div>
                     <div className={styles.minePower}>
-                        <div><p>255 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 21).toFixed(2)} ETL</p></div>
-                        <div><p>250 to 254 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 20).toFixed(2)} ETL</p></div>
-                        <div><p>245 to 249 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 19).toFixed(2)} ETL</p></div>
-                        <div><p>240 to 244 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 18).toFixed(2)} ETL</p></div>
-                        <div><p>235 to 239 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 17).toFixed(2)} ETL</p></div>
-                        <div><p>230 to 234 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 16).toFixed(2)} ETL</p></div>
-                        <div><p>225 to 229 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 15).toFixed(2)} ETL</p></div>
-                        <div><p>220 to 224 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 14).toFixed(2)} ETL</p></div>
-                        <div><p>215 to 219 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 13).toFixed(2)} ETL</p></div>
-                        <div><p>210 to 214 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 12).toFixed(2)} ETL</p></div>
-                        <div><p>205 to 209 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 11).toFixed(2)} ETL</p></div>
-                        <div><p>200 to 204 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 10).toFixed(2)} ETL</p></div>
+                        <div><p>255 MP</p>  <ETLPriceDisplay multiplier={20}/></div>
+                        <div><p>250 to 254 MP</p>  <ETLPriceDisplay multiplier={19.5}/></div>
+                        <div><p>245 to 249 MP</p>  <ETLPriceDisplay multiplier={19}/></div>
+                        <div><p>240 to 244 MP</p>  <ETLPriceDisplay multiplier={18}/></div>
+                        <div><p>235 to 239 MP</p>  <ETLPriceDisplay multiplier={17}/></div>
+                        <div><p>230 to 234 MP</p>  <ETLPriceDisplay multiplier={16}/></div>
+                        <div><p>225 to 229 MP</p>  <ETLPriceDisplay multiplier={15}/></div>
+                        <div><p>220 to 224 MP</p>  <ETLPriceDisplay multiplier={14}/></div>
+                        <div><p>215 to 219 MP</p>  <ETLPriceDisplay multiplier={13}/></div>
+                        <div><p>210 to 214 MP</p>  <ETLPriceDisplay multiplier={12}/></div>
+                        <div><p>205 to 209 MP</p>  <ETLPriceDisplay multiplier={11}/></div>
+                        <div><p>200 to 204 MP</p>  <ETLPriceDisplay multiplier={10}/></div>
                     </div>
                 </div>
                 <div className={styles.rarity}>
                     <div className={styles.rareStar}>⭐⭐⭐⭐</div>
                     <div className={styles.minePower}>
-                        <div><p>200 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 10).toFixed(2)} ETL</p></div>
-                        <div><p>195 to 199 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 9.6).toFixed(2)} ETL</p></div>
-                        <div><p>190 to 194 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 9.2).toFixed(2)} ETL</p></div>
-                        <div><p>185 to 189 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 8.8).toFixed(2)} ETL</p></div>
-                        <div><p>180 to 184 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 8.4).toFixed(2)} ETL</p></div>
-                        <div><p>175 to 179 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 8).toFixed(2)} ETL</p></div>
-                        <div><p>170 to 174 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 7.6).toFixed(2)} ETL</p></div>
-                        <div><p>165 to 169 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 7.2).toFixed(2)} ETL</p></div>
-                        <div><p>160 to 164 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 6.8).toFixed(2)} ETL</p></div>
-                        <div><p>155 to 159 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 6.4).toFixed(2)} ETL</p></div>
-                        <div><p>151 to 154 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 6).toFixed(2)} ETL</p></div>
-                        <div><p>150 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 5.6).toFixed(2)} ETL</p></div>
+                        <div><p>200 MP</p>  <ETLPriceDisplay multiplier={10}/></div>
+                        <div><p>195 to 199 MP</p>  <ETLPriceDisplay multiplier={9.8}/></div>
+                        <div><p>190 to 194 MP</p>  <ETLPriceDisplay multiplier={9.4}/></div>
+                        <div><p>185 to 189 MP</p>  <ETLPriceDisplay multiplier={9}/></div>
+                        <div><p>180 to 184 MP</p>  <ETLPriceDisplay multiplier={8.6}/></div>
+                        <div><p>175 to 179 MP</p>  <ETLPriceDisplay multiplier={8.2}/></div>
+                        <div><p>170 to 174 MP</p>  <ETLPriceDisplay multiplier={7.8}/></div>
+                        <div><p>165 to 169 MP</p>  <ETLPriceDisplay multiplier={7.4}/></div>
+                        <div><p>160 to 164 MP</p>  <ETLPriceDisplay multiplier={7}/></div>
+                        <div><p>155 to 159 MP</p>  <ETLPriceDisplay multiplier={6.6}/></div>
+                        <div><p>151 to 154 MP</p>  <ETLPriceDisplay multiplier={6.2}/></div>
+                        <div><p>150 MP</p>  <ETLPriceDisplay multiplier={5.8}/></div>
                     </div>
                 </div>
                 <div className={styles.rarity}>
                     <div className={styles.rareStar}>⭐⭐⭐</div>
                     <div className={styles.minePower}>
-                        <div><p>150 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 5.6).toFixed(2)} ETL</p></div>
-                        <div><p>145 to 149 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 5.25).toFixed(2)} ETL</p></div>
-                        <div><p>140 to 144 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 4.9).toFixed(2)} ETL</p></div>
-                        <div><p>135 to 139 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 4.55).toFixed(2)} ETL</p></div>
-                        <div><p>130 to 134 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 4.2).toFixed(2)} ETL</p></div>
-                        <div><p>125 to 129 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 3.85).toFixed(2)} ETL</p></div>
-                        <div><p>120 to 124 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 3.5).toFixed(2)} ETL</p></div>
-                        <div><p>115 to 119 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 3.15).toFixed(2)} ETL</p></div>
-                        <div><p>110 to 114 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 2.8).toFixed(2)} ETL</p></div>
-                        <div><p>105 to 109 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 2.45).toFixed(2)} ETL</p></div>
-                        <div><p>101 to 104 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 2.15).toFixed(2)} ETL</p></div>
-                        <div><p>100 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 1.8).toFixed(2)} ETL</p></div>
+                        <div><p>150 MP</p>  <ETLPriceDisplay multiplier={5.8}/></div>
+                        <div><p>145 to 149 MP</p>  <ETLPriceDisplay multiplier={5.6}/></div>
+                        <div><p>140 to 144 MP</p>  <ETLPriceDisplay multiplier={5.2}/></div>
+                        <div><p>135 to 139 MP</p>  <ETLPriceDisplay multiplier={4.8}/></div>
+                        <div><p>130 to 134 MP</p>  <ETLPriceDisplay multiplier={4.4}/></div>
+                        <div><p>125 to 129 MP</p>  <ETLPriceDisplay multiplier={4}/></div>
+                        <div><p>120 to 124 MP</p>  <ETLPriceDisplay multiplier={3.6}/></div>
+                        <div><p>115 to 119 MP</p>  <ETLPriceDisplay multiplier={3.2}/></div>
+                        <div><p>110 to 114 MP</p>  <ETLPriceDisplay multiplier={2.8}/></div>
+                        <div><p>105 to 109 MP</p>  <ETLPriceDisplay multiplier={2.4}/></div>
+                        <div><p>101 to 104 MP</p>  <ETLPriceDisplay multiplier={1.9}/></div>
+                        <div><p>100 MP</p>  <ETLPriceDisplay multiplier={1.8}/></div>
                     </div>
                 </div>
                 <div className={styles.rarity}>
                     <div className={styles.rareStar}>⭐⭐</div>
                     <div className={styles.minePower}>
-                        <div><p>100 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 1.8).toFixed(2)} ETL</p></div>
-                        <div><p>95 to 99 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 1.65).toFixed(2)} ETL</p></div>
-                        <div><p>90 to 94 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 1.5).toFixed(2)} ETL</p></div>
-                        <div><p>85 to 89 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 1.35).toFixed(2)} ETL</p></div>
-                        <div><p>80 to 84 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 1.2).toFixed(2)} ETL</p></div>
-                        <div><p>75 to 79 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 1.05).toFixed(2)} ETL</p></div>
-                        <div><p>70 to 74 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 0.9).toFixed(2)} ETL</p></div>
-                        <div><p>65 to 69 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 0.75).toFixed(2)} ETL</p></div>
-                        <div><p>60 to 64 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 0.6).toFixed(2)} ETL</p></div>
-                        <div><p>55 to 59 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 0.45).toFixed(2)} ETL</p></div>
-                        <div><p>51 to 54 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 0.3).toFixed(2)} ETL</p></div>
-                        <div><p>50 MP</p>  <p className={styles.ETLPriceWorker}>{(mintCost * ADD_TAX * 0.15).toFixed(2)} ETL</p></div>
+                        <div><p>100 MP</p>  <ETLPriceDisplay multiplier={1.8}/></div>
+                        <div><p>95 to 99 MP</p>  <ETLPriceDisplay multiplier={1.75}/></div>
+                        <div><p>90 to 94 MP</p>  <ETLPriceDisplay multiplier={1.5}/></div>
+                        <div><p>85 to 89 MP</p>  <ETLPriceDisplay multiplier={1.25}/></div>
+                        <div><p>80 to 84 MP</p>  <ETLPriceDisplay multiplier={1}/></div>
+                        <div><p>75 to 79 MP</p>  <ETLPriceDisplay multiplier={0.75}/></div>
+                        <div><p>70 to 74 MP</p>  <ETLPriceDisplay multiplier={0.65}/></div>
+                        <div><p>65 to 69 MP</p>  <ETLPriceDisplay multiplier={0.55}/></div>
+                        <div><p>60 to 64 MP</p>  <ETLPriceDisplay multiplier={0.45}/></div>
+                        <div><p>55 to 59 MP</p>  <ETLPriceDisplay multiplier={0.35}/></div>
+                        <div><p>51 to 54 MP</p>  <ETLPriceDisplay multiplier={0.25}/></div>
+                        <div><p>50 MP</p>  <ETLPriceDisplay multiplier={0.2}/></div>
                     </div>
                 </div>
+            </div>
+
+            <div className={styles.footNote}>
+                <em>For bug reports, improvement suggestions and requests, dm me @ discord frifster#1185 </em>
             </div>
         </div>
     )

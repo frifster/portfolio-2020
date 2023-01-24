b2b3960a3b3032f1f4ae2b9757b219b6c9788c45
Eugene Avila <frifster2014@gmail.com>
2023-01-25 05:57:57 +0800

    Added: Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.less'
 
-function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp

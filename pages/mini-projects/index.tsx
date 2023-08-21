6aec794c293f1cba12897409ae13c2c32e8e5cc5
Eugene Avila <frifster2014@gmail.com>
2023-08-21 16:41:20 +0800

    Added less module

--- pages/mini-projects/index.tsx
+++ pages/mini-projects/index.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import Head from 'next/head';
 
 import styles from "../../styles/Projects.module.less";
-import BackgroundParticles from '../../components/BackgroundParticle';
 import Calculator from '../../components/apps/Calculator';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { useRouter } from 'next/router'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+import {useRouter} from 'next/router'
 import ConnectFour from '../../components/apps/ConnectFour';
 
 function Projects() {
@@ -17,8 +16,8 @@ function Projects() {
         <div className={styles.projectsContainer}>
             <Head>
                 <title>Eugene's Side Projects</title>
-                <link rel="icon" href="./icon.png" />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="icon" href="./icon.png"/>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             </Head>
             <div className={styles.projects}>
                 <FontAwesomeIcon
@@ -28,8 +27,8 @@ function Projects() {
                 />
                 <h1 className={styles.title}>Side Projects</h1>
                 <div className={styles.projectsDiv}>
-                    <Calculator />
-                    <ConnectFour />
+                    <Calculator/>
+                    <ConnectFour/>
                 </div>
             </div>
         </div>
